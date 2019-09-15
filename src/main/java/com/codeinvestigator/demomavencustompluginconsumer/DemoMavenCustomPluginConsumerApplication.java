016a9e45f4354886faf32c89d39996b4055fd911
Mike Møller Nielsen <ekim197711@gmail.com>
2019-09-15 21:09:22 +0200

    files

---
+++ src/main/java/com/codeinvestigator/demomavencustompluginconsumer/DemoMavenCustomPluginConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.codeinvestigator.demomavencustompluginconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMavenCustomPluginConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMavenCustomPluginConsumerApplication.class, args);
+    }
+
+}
